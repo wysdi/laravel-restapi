9156497072f61954d3354438fd41069cd286a5bd
Wayan Sudiarta <wayan@tix.com.au>
2023-03-08 21:42:39 +0800

    Create resources and model for Blog

---
+++ app/Models/Blog.php
@@ -0,0 +1,29 @@
+<?php
+
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Auth;
+
+class Blog extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'title',
+        'description'
+    ];
+
+    /**
+     * The "booted" method of the model.
+     *
+     * @return void
+     */
+    protected static function booted()
+    {
+        static::creating(function ($blog) {
+            $blog->user_id = Auth::id();
+        });
+    }
+}
